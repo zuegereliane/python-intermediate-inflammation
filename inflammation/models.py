c66911029b90a59210a8b7232ed3f1d81ec2ab3f
Eliane Züger <122447423+zuegereliane@users.noreply.github.com>
2024-10-15 11:07:04 +0200

    Update inflammation/models.py
    Co-authored-by: Clay Roberts <56594586+Clayton-Roberts@users.noreply.github.com>

--- inflammation/models.py
+++ inflammation/models.py
@@ -33,7 +33,7 @@ def daily_min(data):
     return np.min(data, axis=0)
 
 
-def s_dev(data):
+def s_dev(data) -> dict:
     """Computes and returns standard deviation for data."""
     mmm = np.mean(data, axis=0)
     devs = []
